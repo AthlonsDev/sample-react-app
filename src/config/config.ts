c9714898a7069463fd2f934aacb5881b05a2f4c5
Andrea_Sanna <47662221+AthlonsDev@users.noreply.github.com>
2024-03-18 18:23:18 +0000

    configs algoskd

---
+++ src/config/config.ts
@@ -0,0 +1,18 @@
+import algosdk from "algosdk";
+
+const algodToken = "a".repeat(64);
+const server: string = "http://localhost";
+const port: string = "4001";
+
+const mnemonic: string = 
+"spawn cheese involve stomach drive else side can glad chaos fame embody move harbor boy group game forget demise trouble first lonely card absent valid";
+
+export function getClient(): algosdk.Algodv2 {
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+
+export function getAccount(): algosdk.Account {
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+}
