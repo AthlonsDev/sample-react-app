d4a8f0e2a2c3ba51d905162f6470de2b92edddd4
athlonsdev <athlons91@gmail.com>
2024-03-19 12:54:51 +0000

    fixes

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://sturdy-happiness-r6qj6qv9q9hxvv4-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
